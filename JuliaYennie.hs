9148db07779bacc023a4b5313b3679f04bb554e3
yenniejun <yennie.jun@gmail.com>
2016-10-02 23:31:25 -0400

    Edited comments for fxns

--- JuliaYennie.hs
+++ JuliaYennie.hs
@@ -34,7 +34,8 @@ import Data.List
 
 -}
 
---TYPES
+--------------TYPES--------------
+
 type Dist a = [(Rational, a)]
 
 dEq :: Eq a => Dist a -> Dist a -> Bool
@@ -85,7 +86,7 @@ d12 = uniform [1..12]
 d20 :: Dist Integer
 d20 = uniform [1..20]
 
---Probability distribution of one d6 roll
+-- Probability distribution of one d6 roll
 probabilityQuestionA_d6 :: Dist Integer
 probabilityQuestionA_d6 = d6
 probabilityQuestionA_d12 :: Dist Integer
@@ -94,18 +95,11 @@ probabilityQuestionA_d12 = d12
 
 -------FUNCTIONS FOR CREATING JOINT DISTRIBUTIONS-------
 
---Join two distribution lists using mapping function f
---TODO: only one dist as input? DistElement a, or just a? get rid of Distelement, just arrays. no foldables
---dmap :: ((Rational, a)->(Rational, b)) -> Dist a -> Dist b
---dmap f dist = let Dist xs = dist
---              in Dist (map f xs)
---join d1 d2 f = concat (dmap (f d2) d1)
-
-
+-- Join two distribution lists using mapping function f
 join :: Dist a -> Dist b -> (Dist b -> (Rational, a) -> Dist c) -> Dist c
 join d1 d2 f = concat (map (f d2) d1)
 
---Multiplies probabilities & cons
+-- Multiplies probabilities & cons
 sq :: Dist [a] -> (Rational, [a]) -> Dist [a]
 sq d2s (prob, d) = map (\(a, b) -> (prob*a, b++d)) d2s
 
@@ -113,21 +107,21 @@ sq d2s (prob, d) = map (\(a, b) -> (prob*a, b++d)) d2s
 sqdiff :: Dist a -> (Rational, b) -> Dist (a,b)
 sqdiff d2s (p1, b) = map (\(p2, a) -> (p1*p2, (a,b))) d2s
 
---D2 distribution depends on D1 value
+-- D2 distribution depends on D1 value
 dep :: (Eq b, Num b) => Dist [a] -> (Rational, b) -> Dist [a]
 dep d2s (p1, 1) = map (\(p2, x) ->  (p1*p2, x)) d2s
 dep d2s (p, n) = join d2s (dep d2s (p, n-1)) sq
 
---Adding two values
+-- Adding two values
 add :: Num a => Dist a -> (Rational, a) -> Dist a
 add d2s (p1, x) = map (\(p2, y) -> (p1*p2, x+y)) d2s
 
---Join a distribution with itself n times
+-- Join a distribution with itself n times
 multiple_join :: Dist [a] -> Integer -> Dist [a]
 multiple_join dist 1 = dist
 multiple_join dist n = multiple_join (join dist dist sq) (n-1)
 
---A join specifically for adding rolling two dice together
+-- A join specifically for adding rolling two dice together
 dice_tup :: Num b => Dist (a, Dist b) -> (Rational, (a, Dist b)) -> Dist (a, a, Dist b)
 dice_tup d2s (p1, (die1,dist1)) =
     map (\(p2, (die2,dist2)) -> (p1*p2, (die1,die2,(join dist1 dist2 add)))) d2s
@@ -136,11 +130,11 @@ dice_tup d2s (p1, (die1,dist1)) =
 
 ------EXAMPLES OF JOINT DISTRIBUTIONS------
 
---Joint distribution of two coin flips
+-- Joint distribution of two coin flips
 cxc :: Dist [Coin]
 cxc = join coinlist coinlist sq
 
---Joint distribution of a d6 roll and coin flips that depend on the outcome
+-- Joint distribution of a d6 roll and coin flips that depend on the outcome
 distd6 :: Dist [Coin]
 distd6 = join d6 coinlist dep
 
@@ -148,49 +142,48 @@ distd6 = join d6 coinlist dep
 -------FILTERING A DISTRIBUTION-------
 
 -- Determine how many heads/tails are flipped in the list  
--- Coin -> Coin list -> int 
 determine :: Coin -> [Coin] -> Int 
 determine ht coinlist = foldl (\acc x -> if (x==ht) then acc + 1 else acc) 0 coinlist  
 
--- Int -> Coin -> Dist list -> Dist list 
+-- Make a coin distribution list based on how many of the flips were heads/tails
 makeFinalList :: Int -> Coin -> Dist [Coin] -> Dist [Coin]
 makeFinalList numFlips flipType dlist = filter (\(_, flipList) -> (determine flipType flipList == numFlips)) dlist
 
--- Adds all of the probabilities of flipping numFlips Heads/Tails
--- Int -> Coin -> Dist List -> Rational
+-- Adds all of the probabilities of flipping numFlips amount of heads/tails
 probSum :: Int -> Coin -> Dist [Coin] -> Rational
 probSum numFlips flipType distType = toRational (sum (map (\(prob, _) -> prob) (makeFinalList numFlips flipType distType)))
 
---The probability of seeing 3 heads when a d6 roll determines the number of coin tosses
--- Rational
+-- The probability of seeing 3 heads when a d6 roll determines the number of coin tosses
 probabilityQuestionF :: Rational
 probabilityQuestionF = probSum 3 Heads distd6
 
 -------TALLY SHEET-------
 
---Pot of dice distribution
+-- Pot of dice distribution
 pot :: Dist (Die, Dist Integer)
 pot = [((9%46), (D6,d6)), ((9%46), (D8,d8)), ((14%46), (D12,d12)), ((14%46), (D20,d20))]
 
---WE ARE DOING WITH REPLACEMENT 
---Probability of drawing any two dice
+-- Probability of drawing any two dice
+-- Note: We are doing this WITH replacement 
 probDraw :: Dist (Die, Die, Dist Integer)
 probDraw = let draw2 = join pot pot dice_tup
             in map (\(prob, (d1, d2, dist)) -> (prob, (d1, d2, (mergeEvents sameEvent dist)))) draw2
 
---Probability of drawing two d12s, filtered to decrease problem size
+-- Probability of drawing two d12s, filtered to decrease problem size
 twoD12s :: Dist (Die, Die, Dist Integer)
 twoD12s = filter (\(_ ,(a,b,_)) -> ((a==D12) && (b == D12))) probDraw
 
+-- Use pattern matching to retrieve necessary data 
 d12Prob :: Rational
 b :: (Die, Die, Dist Integer)
 (d12Prob, b) = head twoD12s
 
+-- Distribution list of two D12 dice
 twoD12s_rollProbs :: Dist Integer
 twoD12s_rollProbs = join d12 d12 add
 
 
---Functions to use with mergeEvents
+-- Functions to use with mergeEvents
 sameEvent :: Eq a => (Rational, a) -> (Rational, a) -> Bool
 sameEvent (_, x) (_, y) = x==y
 sameTally :: (Eq a, Eq a1, Eq a2) => (Rational, (a, a1, a2)) -> (Rational, (a, a1, a2)) -> Bool
@@ -198,7 +191,7 @@ sameTally (_, (l1, e1, g1)) (_, (l2, e2, g2)) = (l1==l2)&&(e1==e2)&&(g1==g2)
 sameDicePair :: Eq a => (Rational, (a, a, t)) -> (Rational, (a, a, t1)) -> Bool
 sameDicePair (_, (d_a1, d_a2, _)) (_, (d_b1, d_b2, _)) = ((d_a1==d_b1)&&(d_a2==d_b2))||((d_a1==d_b2)&&(d_a2==d_b1))
 
---For merging distribution Nodes with the same value
+-- For merging distribution Nodes with the same value
 mergeEvents :: ((Rational, a) -> (Rational, a) -> Bool) -> Dist a -> Dist a
 mergeEvents f xs = case xs' of
              [] -> []
@@ -220,34 +213,39 @@ compactD12s = mergeEvents sameEvent twoD12s_rollProbs
 d20s_list :: Dist [Integer]
 d20s_list = listify compactD12s
 
---Distribution of two d12's rolled 30 times
+-- Distribution of two d12's rolled 30 times
 d12s_30rolls :: Dist [Integer]
 d12s_30rolls = multiple_join d20s_list 30
 
-testlist :: Dist [Integer]
-testlist = [(1%2, [2 .. 24]), (1%2, [16..18]), (1%2, [19..21]), (1%2, [19..22])]
 
--- Int List -> Operator -> Int 
--- compareSixteen :: (Num b, Num a1, Foldable t) => t a -> (a -> a1 -> Bool) -> b
+-- For the purposes of testing 
+-- testlist :: Dist [Integer]
+-- testlist = [(1%2, [2 .. 24]), (1%2, [16..18]), (1%2, [19..21]), (1%2, [19..22])]
+
+-- For the given prefix operator (i.e. ==, <, >), figure out how many in the given list
+-- are under/over/equal to the number 16
 compareSixteen :: [Integer] -> (Integer -> Integer -> Bool) -> Integer 
 compareSixteen sumList op = foldl(\acc x -> if (op x 16) then acc + 1 else acc) 0 sumList
 
--- Int -> Dist List -> Operator -> Int 
+-- Filter the given sum list for those that conform to the given prefix operator
 determineHowMany :: Integer -> Dist [Integer] -> (Integer -> Integer -> Bool) -> Dist [Integer] 
 determineHowMany numOverUnder dlist op = filter (\(_, sumList) -> compareSixteen sumList op == numOverUnder) dlist
-{-
--- Adds: probability of the two D12s added PLUS probabilty of drawing 2 D12s
--- Int -> Dist List -> Operator -> Rational
--- tallyProb :: (Num a2, Foldable t) => Integer -> [DistElement (t a1)] -> (a1 -> a2 -> Bool) -> Rational
+
+-- Adds the probability of the two D12s added PLUS probabilty of drawing 2 D12s
+tallyProb :: Integer -> Dist [Integer] -> (Integer -> Integer -> Bool) -> Rational
 tallyProb numOverUnder dlist op = toRational(sum (map(\(prob, _)->prob)(determineHowMany numOverUnder dlist op))) + d12Prob
+
+
 
 -- Rational
 tallyProbabilityN :: Rational
 tallyProbabilityN = tallyProb 3 d12s_30rolls (>)
 tallyProbabilityO :: Rational
 tallyProbabilityO = tallyProb 16 d12s_30rolls (<)
---NOTE: theoretically, tallyProbabilityN is the correct probability. However, we could not get the calculation to terminate
--}
+
+-- NOTE: theoretically, tallyProbabilityN is the correct probability. 
+-- However, we could not get the calculation to terminate
+
 -- Dist List of probability and tuple (numUnder16, numEqual16, numOver16)
 createTallySheet :: Dist [Integer] -> Dist (Integer, Integer, Integer)
 createTallySheet dist = 
